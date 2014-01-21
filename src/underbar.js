4960d866b89a45134e0a24e1798c436ea94f01f1
Sarith21 <sarith21@gmail.com>
2014-01-21 15:09:54 +1100

    Fixed OBOB on line 35; _.last now working.

--- src/underbar.js
+++ src/underbar.js
@@ -29,7 +29,10 @@ var _ = { };
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
-    return n === undefined ? array[0] : array.slice(array.length -n, array.length);
+    if (array.length < n)
+      return array;
+    else
+      return n === undefined ? array[array.length -1] : array.slice(array.length -n, array.length);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
